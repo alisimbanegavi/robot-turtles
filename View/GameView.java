690efb6d9a70386502ff429eaec685cabffe0f2b
Zyphu <cumming.romo.daniel@gmail.com>
2020-10-27 15:19:38 -0300

    Add files via upload
    Still changing other txt but simpilfying the view

--- View/GameView.java
+++ View/GameView.java
@@ -4,9 +4,9 @@ package View;
  * GameView class for display
  */
 public class GameView {
-    public void displayWelcome (){
-        System.out.println("Welcome to Robot Turtles");
-    }
+    // public void displayWelcome (){
+    //     System.out.println("Welcome to Robot Turtles");
+    // }
 
     public void displayGoodbye() {
         System.out.println("Thanks for playing!");
@@ -18,6 +18,10 @@ public class GameView {
 
     public void displayWinner (String name, String turtleColour){
         System.out.println ("Congratulations "+name+"!\nYour "+turtleColour+" Turtle has captured a Jewel. You have WON!!!");
+    }
+
+    public void displayText(String txt){
+        System.out.println(txt);
     }
 
     public void displayPrompt (String prompt){
