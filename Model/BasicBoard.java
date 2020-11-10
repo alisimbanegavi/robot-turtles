471f0792df7b9f6b69f6536de47d05951e77f8b8
Ali <61428758+alisimbanegavi@users.noreply.github.com>
2020-11-09 23:10:18 -0400

    Delete BasicBoard.java

--- Model/BasicBoard.java
+++
@@ -1,9 +0,0 @@
-package Model;
-
-import java.util.List;
-
-public class BasicBoard extends Board {
-    BasicBoard(int n, List<Turtle> players, List<Tile> jwls){
-        super(n, players, jwls);
-    }    
-}
