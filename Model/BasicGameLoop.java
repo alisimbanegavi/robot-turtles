b6903088ddff6a22029343414cdb84828866f5d2
Ali <61428758+alisimbanegavi@users.noreply.github.com>
2020-11-09 23:10:36 -0400

    Delete BasicGameLoop.java

--- Model/BasicGameLoop.java
+++
@@ -1,9 +0,0 @@
-package Model;
-
-import Controller.GameController;
-
-public class BasicGameLoop extends GameLoop{
-    public BasicGameLoop(GameController controller){
-        super(controller);
-    }
-}
